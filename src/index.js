285bca3c29a87c9ae0efdb64cb2f83f9df299476
Leooeloel <1826234293@qq.com>
2022-06-30 15:31:44 +0800

    adjust note

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const dices = [null, dice_1, dice_2, dice_3, dice_4, dice_5, dice_6];
 /** @type {import("@netless/window-manager").NetlessApp} */
 const App_dice = {
   kind: "Dice",
+  // 配置默认窗口大小
   config: {
     minwidth: 0.2,
     minheight: 0.2,
@@ -21,18 +22,18 @@ const App_dice = {
     height: 0.35,
   },
   setup(context) {
+    // box 为插件所在的窗口
     const box = context.getBox();
     box.mountStyles(styles);
-
+    // 创建一个 DOM 用来放内容
     const $content = document.createElement("div");
     $content.className = "app-dice";
     box.mountContent($content);
 
-    const storage = context.storage;
-    storage.ensureState({
-      src: dice_1,
-    });
+    // 创建并连接上同步服务 storage
+    const storage = context.createStorage("App_dice", { src: dice_1 });
 
+    // 骰子图片
     const $img = document.createElement("img");
     $img.className = "dice-img";
     $img.src = storage.state.src;
@@ -42,7 +43,7 @@ const App_dice = {
       shake();
     };
 
-    // 生成随机数
+    // 生成指定区间内的随机数
     function rand(min, max) {
       if (min > max) {
         var mid = min;
@@ -52,26 +53,29 @@ const App_dice = {
       return parseInt(Math.random() * (max - min + 1) + min);
     }
 
-    //点击事件
+    // 点击事件
     function shake() {
+      // 骰子正在转动中，点击不生效
       if (storage.state.src != shake_7) {
-        //若不在转动中，则执行下边的内容
+        // 保存动态图状态到同步服务
         storage.setState({ src: shake_7 });
 
-        //随机 1～3 秒后，将动态图替换为随机点数的图片
+        // 随机 1～2 秒后，将动态图替换为随机点数的图片
         var timer = setTimeout(function () {
+          // 更新点数图片到状态到同步服务
           storage.setState({ src: dices[rand(1, 6)] });
         }, rand(1000, 2000));
       }
     }
 
-    //监听到 storage 变化就刷新视图
     function refresh() {
       $img.src = storage.state.src;
     }
+    //监听到 storage 变化就刷新视图
     const dispose = storage.addStateChangedListener(refresh);
     refresh();
 
+    //关闭插件的时候销毁未使用的侦听器
     context.emitter.on("destroy", () => {
       dispose();
     });
